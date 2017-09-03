eefc7c797f8f7aa36faf3324434fa21dd357cce7
Quinn Weber <quinn@quinnweber.com>
2017-09-02 23:09:26 -0700

    add missing init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='radio-export',
-    version='0.0.1',
+    version='0.0.2',
     description='fill in',
     author='Quinn Weber',
     maintainer='Quinn Weber',

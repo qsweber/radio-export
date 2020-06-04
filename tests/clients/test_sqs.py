60e8aacdd4239b5a1548158bf5a7c7e1b3bf8a8c
quinn <quinn@quinnweber.com>
2020-06-04 16:36:42 -0700

    fix test

--- tests/clients/test_sqs.py
+++ tests/clients/test_sqs.py
@@ -3,7 +3,48 @@ import json
 import uuid
 
 import service_template.clients.sqs as module
-from tests.app.requests.sqs_message import test_input, test_output
+
+test_input = {
+    "Records": [
+        {
+            "messageId": "8aa50ffc-1df9-4981-9ef1-46a7878f89dd",
+            "receiptHandle": "foo",
+            "body": '{"team_id": "1", "channel_id": "2", "user_id": "abc", "text": "def"}',
+            "attributes": {
+                "ApproximateReceiveCount": "1",
+                "SentTimestamp": "1583293981975",
+                "SenderId": "120356305272",
+                "ApproximateFirstReceiveTimestamp": "1583293982030",
+            },
+            "messageAttributes": {},
+            "md5OfBody": "c72b9698fa1927e1dd12d3cf26ed84b2",
+            "eventSource": "aws:sqs",
+            "eventSourceARN": "arn:aws:sqs:us-west-2:120356305272:dev_test",
+            "awsRegion": "us-west-2",
+        },
+    ],
+}
+
+test_output = [
+    module.SqsMessage(
+        message_id=uuid.UUID("8aa50ffc-1df9-4981-9ef1-46a7878f89dd"),
+        receipt_handle="foo",
+        body={"team_id": "1", "channel_id": "2", "user_id": "abc", "text": "def"},
+        attributes=module.SqsMessageAttributes(
+            approximate_receive_count=1,
+            sent_timestamp=datetime.datetime(2020, 3, 4, 3, 53, 1, 975000),
+            sender_id="120356305272",
+            approxmate_first_receive_timestamp=datetime.datetime(
+                2020, 3, 4, 3, 53, 2, 30000
+            ),
+        ),
+        md5_of_body="c72b9698fa1927e1dd12d3cf26ed84b2",
+        event_source="aws:sqs",
+        event_source_arn="arn:aws:sqs:us-west-2:120356305272:dev_test",
+        event_source_url="https://sqs.us-west-2.amazonaws.com/120356305272/dev_test",
+        aws_region="us-west-2",
+    )
+]
 
 
 def test_json_round_trip_encoding():

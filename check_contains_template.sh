c3f7e5704bb1d65cc29703c18020b2fe490c5fed
Quinn Weber <quinn@quinnweber.com>
2023-02-23 09:40:28 -0800

    fix check_template

--- check_contains_template.sh
+++ check_contains_template.sh
@@ -1,1 +1,1 @@
-TERM=xterm-256color git branch --contains $(git ls-remote git://github.com/qsweber/service-template.git | grep refs/heads/master | cut -f 1)
+TERM=xterm-256color git branch --contains $(git ls-remote https://github.com/qsweber/service-template.git | grep refs/heads/master | cut -f 1)
